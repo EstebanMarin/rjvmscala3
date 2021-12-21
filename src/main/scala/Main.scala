ca1b77992a21f038c22076f77f5d77446cd3340a
esteban <esteban.marin@adidas.com>
2021-12-21 15:09:51 -0500

    more inform

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+@main def hello: Unit =
+  class Animal:
+    val creatureType = "wild"
+    def eat(): Unit = println("nomonm")
 
-def msg = "I was compiled by Scala 3. :)"
+  class Cat extends Animal:
+    def crounch() =
+      eat()
+      println("Crounch, Crounch")
+
+  val cat = new Cat
+
+  class Person(val name: String, age: Int):
+    def this(name: String) = this(name, 0)
+
+  class Adult(name: String, age: Int, idCard: String) extends Person(name, age)
+
+  class Dog extends Animal:
+    override val creatureType = "domestic"
+    override def eat(): Unit = super.eat()
+    override def toString: String = "a dog"
+
+  //subtype poliphormism
+  val dog: Animal = new Dog
+  dog.eat()
