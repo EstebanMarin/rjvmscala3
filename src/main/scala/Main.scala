b66ab67990ba5cb7264d9e7cac63c4855b981731
esteban <esteban.marin@adidas.com>
2021-12-21 15:52:48 -0500

    more

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -22,4 +22,13 @@
 
   //subtype poliphormism
   val dog: Animal = new Dog
-  dog.eat()
+
+  class Crocodile extends Animal:
+    override val creatureType = "very wild"
+    override def eat(): Unit = println("I can eat anything, I'm a Croc")
+    def eat(animal: Animal): Unit = println(s"eating this pour")
+    def eat(dog: Dog): Unit = println("Eating dog")
+    def eat(person: Person): Unit = println(s"Eating ${person.name}")
+    // def eat(person: Person): Int = 45
+    def eat(person: Person, animal: Animal): Int = 45
+// def eat(person: Person, animal: Animal): Unit = println("It works ")
