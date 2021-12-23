eb56145a24d37769bdfeb4b6c9618f7f50abdc2e
esteban <esteban.marin@adidas.com>
2021-12-23 14:22:52 -0500

    more

--- src/main/scala/Enums.scala
+++ src/main/scala/Enums.scala
@@ -1,5 +1,23 @@
-object Enums {
-  enum Permisions {
+object Enums:
+  enum Permissions:
     case READ, WRITE, EXECUTE, NONE
+    def openDocument(): Unit =
+      if (this == READ) println("opening document ... ")
+      else println("reading document")
+
+  val somePermissions: Permissions = Permissions.READ
+
+  enum PermissionsWithBits(bits: Int) {
+    case READ extends PermissionsWithBits(4)
+    case WRITE extends PermissionsWithBits(2)
+    case EXECUTE extends PermissionsWithBits(1)
+    case NONE extends PermissionsWithBits(0)
   }
-}
+
+  object PermissionsWithBits {
+    def fromBits(bits: Int): PermissionsWithBits =
+      PermissionsWithBits.NONE
+  }
+
+@main def enumsMain =
+  println("Hello from enums")
