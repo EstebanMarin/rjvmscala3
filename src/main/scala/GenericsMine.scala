14b7fac853147ffca259c416fda325dae2daf6fd
esteban <esteban.marin@adidas.com>
2021-12-23 12:07:06 -0500

    more

--- src/main/scala/GenericsMine.scala
+++ src/main/scala/GenericsMine.scala
@@ -1,36 +1,53 @@
 import scala.annotation.tailrec
-import GenericsMine.MyList
+
 object GenericsMine {
-  abstract class MyList[A]:
+  abstract class LList[A]:
     def head: A
-    def tail: MyList[A]
+    def tail: LList[A]
     val isEmpty: Boolean
-    infix def +(element: A): MyList[A] = new NonEmpty(element, this)
-    def map[B](transformer: Transformer[A, B]): MyList[B]
+    infix def +(element: A): LList[A] = new NonEmpty(element, this)
+    infix def ++(anotherList: LList[A]): LList[A]
+    def map[B](transformer: Transformer[A, B]): LList[B]
+    def filter(predicate: Predicate[A]): LList[A]
+    def flatMap[B](transformer: Transformer[A, LList[B]]): LList[B]
 
-  class Empty[A] extends MyList[A] {
+  class Empty[A] extends LList[A] {
     override def head: A = throw new NoSuchElementException
-    override def tail: MyList[A] = throw new NoSuchElementException
+    override def tail: LList[A] = throw new NoSuchElementException
     override val isEmpty: Boolean = true
-
-    override def map[B](transformer: Transformer[A, B]): MyList[B] =
+    override def map[B](transformer: Transformer[A, B]): LList[B] =
       new Empty[B]
+    override def filter(predicate: Predicate[A]): LList[A] =
+      this
+
+    override def ++(anotherList: LList[A]): LList[A] = anotherList
+    override def flatMap[B](transformer: Transformer[A, LList[B]]): LList[B] =
+      new Empty[B]
+
   }
 
-  class NonEmpty[A](override val head: A, override val tail: MyList[A])
-      extends MyList[A]:
+  class NonEmpty[A](override val head: A, override val tail: LList[A])
+      extends LList[A]:
     override val isEmpty = false
-    override def map[B](transformer: Transformer[A, B]): MyList[B] =
+    override def map[B](transformer: Transformer[A, B]): LList[B] =
       new NonEmpty(transformer.transform(head), tail.map(transformer))
 
-    override def toString = {
+    override def filter(predicate: Predicate[A]): LList[A] =
+      if (predicate.test(head)) new NonEmpty(head, tail.filter(predicate))
+      else tail.filter(predicate)
+
+    override def ++(anotherList: LList[A]): LList[A] =
+      new NonEmpty(head, tail ++ anotherList)
+    override def flatMap[B](transformer: Transformer[A, LList[B]]): LList[B] =
+      transformer.transform(head) ++ tail.flatMap(transformer)
+
+    override def toString =
       @tailrec
-      def concatenateElements(reminder: MyList[A], acc: String): String =
+      def concatenateElements(reminder: LList[A], acc: String): String =
         if (reminder.isEmpty) acc
         else concatenateElements(reminder.tail, s"${acc} ${reminder.head}")
 
       s"[${concatenateElements(this, "")} ]"
-    }
 
   trait Predicate[T]:
     def test(x: T): Boolean
@@ -46,12 +63,12 @@ object GenericsMine {
     override def transform(x: Int) = x * 2
   }
 
-  class DoublerList extends Transformer[Int, MyList[Int]] {
-    override def transform(x: Int): MyList[Int] =
+  class DoublerList extends Transformer[Int, LList[Int]] {
+    override def transform(x: Int): LList[Int] =
       new NonEmpty(x, new NonEmpty(x + 1, new Empty))
   }
 
-  val listofInterger: MyList[Int] =
+  val listofInterger: LList[Int] =
     new Empty + 1 + 2 + 3
 
   @main def genericsMain =
@@ -59,11 +76,17 @@ object GenericsMine {
     val evenPredicate = new Predicate[Int]:
       override def test(x: Int) = x % 2 == 0
 
-    val listofInterger: MyList[Int] =
+    val listofInterger: LList[Int] =
       new Empty + 1 + 2 + 3
 
     val doubledInt = listofInterger.map(new Doubler)
     println(doubledInt)
+
+    val doubledList = listofInterger.map(new DoublerList)
+    println(doubledList)
+
+    val evenNumbers = listofInterger.filter(new EvenPredicate)
+    println(evenNumbers)
 
     println("-" * 50)
 }
