dfd2ca5562b29094938f5c6812df550b9e93eea0
esteban <esteban.marin@adidas.com>
2021-12-23 11:33:23 -0500

    more

--- src/main/scala/GenericsMine.scala
+++ src/main/scala/GenericsMine.scala
@@ -1,27 +1,35 @@
 import scala.annotation.tailrec
+import GenericsMine.MyList
 object GenericsMine {
-  abstract class MyList[T]:
-    def head: T
-    def tail: MyList[T]
+  abstract class MyList[A]:
+    def head: A
+    def tail: MyList[A]
     val isEmpty: Boolean
-    infix def +(element: T): MyList[T] = new NonEmpty(element, this)
+    infix def +(element: A): MyList[A] = new NonEmpty(element, this)
+    def map[B](transformer: Transformer[A, B]): MyList[B]
 
-  class Empty[T] extends MyList[T] {
-    override def head: T = throw new NoSuchElementException
-    override def tail: MyList[T] = throw new NoSuchElementException
+  class Empty[A] extends MyList[A] {
+    override def head: A = throw new NoSuchElementException
+    override def tail: MyList[A] = throw new NoSuchElementException
     override val isEmpty: Boolean = true
+
+    override def map[B](transformer: Transformer[A, B]): MyList[B] =
+      new Empty[B]
   }
 
-  class NonEmpty[T](override val head: T, override val tail: MyList[T])
-      extends MyList[T]:
+  class NonEmpty[A](override val head: A, override val tail: MyList[A])
+      extends MyList[A]:
     override val isEmpty = false
+    override def map[B](transformer: Transformer[A, B]): MyList[B] =
+      new NonEmpty(transformer.transform(head), tail.map(transformer))
+
     override def toString = {
       @tailrec
-      def concatenateElements(reminder: MyList[T], acc: String): String =
+      def concatenateElements(reminder: MyList[A], acc: String): String =
         if (reminder.isEmpty) acc
         else concatenateElements(reminder.tail, s"${acc} ${reminder.head}")
 
-      s"[${concatenateElements(this, "")}]"
+      s"[${concatenateElements(this, "")} ]"
     }
 
   trait Predicate[T]:
@@ -34,13 +42,28 @@ object GenericsMine {
   trait Transformer[A, B]:
     def transform(x: A): B
 
+  class Doubler extends Transformer[Int, Int] {
+    override def transform(x: Int) = x * 2
+  }
+
+  class DoublerList extends Transformer[Int, MyList[Int]] {
+    override def transform(x: Int): MyList[Int] =
+      new NonEmpty(x, new NonEmpty(x + 1, new Empty))
+  }
+
   val listofInterger: MyList[Int] =
     new Empty + 1 + 2 + 3
 
   @main def genericsMain =
     println("-" * 50)
-    val evenPredicate = new Predicate[Int] {
+    val evenPredicate = new Predicate[Int]:
       override def test(x: Int) = x % 2 == 0
-    }
+
+    val listofInterger: MyList[Int] =
+      new Empty + 1 + 2 + 3
+
+    val doubledInt = listofInterger.map(new Doubler)
+    println(doubledInt)
+
     println("-" * 50)
 }
