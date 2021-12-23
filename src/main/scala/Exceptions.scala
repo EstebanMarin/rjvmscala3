ca79148dbd0d4c0b864ef5efb84debb20109185a
esteban <esteban.marin@adidas.com>
2021-12-23 14:50:58 -0500

    more

--- src/main/scala/Exceptions.scala
+++ src/main/scala/Exceptions.scala
@@ -1,3 +1,15 @@
-object Exceptions {
-  
-}
+object Exceptions:
+  val aWierdValue: Int = throw new java.lang.NullPointerException
+
+  def getInt(withExpections: Boolean): Int =
+    if (withExpections) throw new RuntimeException("No Int for you")
+    else 42
+
+  val potentialfail: Int =
+    try getInt(true)
+    catch case e: RuntimeException => 54
+
+end Exceptions
+
+@main def ExceptionsMain =
+  println("Hello from Exceptions")
