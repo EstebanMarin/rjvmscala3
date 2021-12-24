caf07ad81a64b5c3e4a503928d9e27bf1d786ad6
esteban <esteban.marin@adidas.com>
2021-12-24 10:46:35 -0500

    more

--- src/main/scala/part2/LinearCollections.scala
+++ src/main/scala/part2/LinearCollections.scala
@@ -1,15 +1,28 @@
 package part2
 
 object LinearCollections:
-  val aSequence = Seq(1, 2, 3)
-  // usefull API
-  val reversed = aSequence.reverse
-  val concatenated = aSequence ++ Seq(1, 2, 3)
-  val sortedSequence = aSequence.sorted
-  // map, flatMap, filter
+  // Sequence
+  object Sequence:
+    val aSequence = Seq(1, 2, 3)
+    // usefull API
+    val head = aSequence.head
+    val tail = aSequence.tail
+    val reversed = aSequence.reverse
+    val concatenated = aSequence ++ Seq(1, 2, 3)
+    val sortedSequence = aSequence.sorted
+    // map, flatMap, filter
+    val anIncrementedSequence = aSequence.map(_ + 1)
+    val aFlatedSequence = aSequence.flatMap(x => Seq(x, x + 1))
+    val aFilteredSequence = aSequence.filter(_ % 2 == 0)
+  end Sequence
 
-  val anIncrementedSequence = aSequence.map(_ + 1)
-  val aFlatedSequence = aSequence.flatMap(x => Seq(x, x + 1))
-  val aFilteredSequence = aSequence.filter(_ % 2 == 0)
+  object ScalaList:
+    // Same as Seq
+    val aList = List(1, 2, 3)
+    // appending and prependding
+    val aBiggerList = 0 +: aList :+ 4
+
+  // List
   @main def linearMain =
-    println(aFlatedSequence)
+    // println(Sequence.aFlatedSequence)
+    println(ScalaList.aBiggerList)
