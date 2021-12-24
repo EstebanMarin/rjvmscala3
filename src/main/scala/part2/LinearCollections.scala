099c3aa0f1146e959f725e7700464537d0018552
esteban <esteban.marin@adidas.com>
2021-12-24 11:42:57 -0500

    more

--- src/main/scala/part2/LinearCollections.scala
+++ src/main/scala/part2/LinearCollections.scala
@@ -12,17 +12,57 @@ object LinearCollections:
     val sortedSequence = aSequence.sorted
     // map, flatMap, filter
     val anIncrementedSequence = aSequence.map(_ + 1)
-    val aFlatedSequence = aSequence.flatMap(x => Seq(x, x + 1))
     val aFilteredSequence = aSequence.filter(_ % 2 == 0)
+    val aFlatedSequence = aSequence.flatMap(x => Seq(x, x + 1))
+    val sum = aSequence.foldLeft(0)(_ + _)
+    val stringRep = aSequence.mkString("[", ",", "]")
   end Sequence
 
-  object ScalaList:
+  object Lists:
     // Same as Seq
     val aList = List(1, 2, 3)
     // appending and prependding
     val aBiggerList = 0 +: aList :+ 4
+    val prepending = 0 :: aList
+    val scalax5 = List.fill(5)("Scala")
+  end Lists
 
-  // List
+  object Ranges:
+    val aRange = 1 to 10
+    (1 to 10).foreach(_ => println("Scala"))
+  end Ranges
+
+  object Arrays:
+    // main capability is mutable
+    val anArray = Array(1, 2, 3, 4, 5, 6)
+    val anSequence = anArray.toIndexedSeq
+    anArray.update(2, 22)
+  end Arrays
+
+  object Vectors:
+    // fast Sequ for large amount of data
+    val aVector = Vector(1, 2, 3, 4, 5, 6)
+  end Vectors
+
+  object Sets:
+    //main API test if it is in Set
+    val aSet = Set(1, 2, 3, 4, 5, 6) // no garanties of ordering
+    val contains = aSet.contains(3)
+    // removing from set
+    val reducedSet = aSet - 4
+    val biggerSet = aSet + 4
+    val concatenated = aSet ++ Set(8, 9, 75, 8)
+    val anotherSet = Set(8, 9, 75, 8)
+    val concatenated_v2 = aSet | anotherSet
+    val aDiffSet = aSet.diff(anotherSet)
+    val aDiffSet_V2 = aSet -- anotherSet
+    // intersection
+    val intersecion = aSet.intersect(anotherSet)
+    val intersecion_v2 = aSet & anotherSet
+
+  end Sets
+
   @main def linearMain =
     // println(Sequence.aFlatedSequence)
-    println(ScalaList.aBiggerList)
+    println(Lists.aBiggerList)
+    println(Sequence.stringRep)
